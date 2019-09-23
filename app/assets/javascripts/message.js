1957a06f485fe548d9ae831a41cf622d3d7165bb
una7 <2ne1.jeremy@gmail.com>
2019-09-23 21:12:01 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,47 @@
-$(function(){
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr("action");
-    $.ajax({
-      url: url,
-      type:"POST",
-      data: formData,
-      dataType: "json",
-      processData: false,
-      contentType: false
-    })
-    .done(function(message){
-      console.log(message);
-    })
-    .fail(function(){
+$(function(){ 
+  function buildHTML(message){
+    var img = message.image ? `<img src=${ message.image }>` : "";
+    var html =`<div class="message">
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="lower-message">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+                  ${img}
+                </div>
+              </div>`
+            return html;
+      }
 
-    })
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr("action");
+  $.ajax({
+    url: url,
+    type:"POST",
+    data: formData,
+    dataType: "json",
+    processData: false,
+    contentType: false
   })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    $('form')[0].reset();
+    console.log(data);
+  })
+  .fail(function(){
+    alert('error');
+  });
+  return false;
+});
 });
