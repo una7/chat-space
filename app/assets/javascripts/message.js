8f7727113fb59d996317d9ab2a6a977d431c2962
una7 <2ne1.jeremy@gmail.com>
2019-10-03 18:44:45 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,33 +45,33 @@ $('#new_message').on('submit', function(e){
 });
 
 
-// var reloadMessages = function() {
-//   console.log("aa")
-//   if (window.location.href.match(/\/groups\/\d+\/messages/)){    // group/:group_id/messagesというURLの時だけ、以降の記述が実行される。
-//   var href = 'api/messages#index {:format=>"json"}'              // リクエスト先と形式を指定する。
-//   var last_message_id = $('.message:last').data('message-id');   // カスタムデータ属性を利用して、最新のメッセージIDを取得する。
+var reloadMessages = function() {
+  console.log("aa")
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){    // group/:group_id/messagesというURLの時だけ、以降の記述が実行される。
+  var href = 'api/messages#index {:format=>"json"}'              // リクエスト先と形式を指定する。
+  var last_message_id = $('.message:last').data('message-id');   // カスタムデータ属性を利用して、最新のメッセージIDを取得する。
   
-//   $.ajax({
-//     url: href,    //↑でパスを「href」にしている。ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-//     type: 'get',    //ルーティングで設定した通りhttpメソッドをgetに指定
-//     dataType: 'json',
-//     data: {id: last_message_id}   //dataオプションでリクエストに値を含める
-//   })
-//   .done(function(messages) {
-//     var insertHTML = '';    //追加するHTMLの入れ物を作る
-//     messages.forEach(function(message) {    //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-//       insertHTML = buildHTML(message);
-//         $('.messages').append(insertHTML);    //メッセージが入ったHTMLを取得
-//         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');    //メッセージを追加
-//   })
-// })
+  $.ajax({
+    url: href,    //↑でパスを「href」にしている。ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    type: 'get',    //ルーティングで設定した通りhttpメソッドをgetに指定
+    dataType: 'json',
+    data: {id: last_message_id}   //dataオプションでリクエストに値を含める
+  })
+  .done(function(messages) {
+    var insertHTML = '';    //追加するHTMLの入れ物を作る
+    messages.forEach(function(message) {    //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);    //メッセージが入ったHTMLを取得
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');    //メッセージを追加
+  })
+})
 
-//   .fail(function() {
-//     alert('自動更新に失敗しました');
-//   });
+  .fail(function() {
+    alert('自動更新に失敗しました');
+  });
 
 
-// };
-// }
-  // setInterval(reloadMessages, 5000);
+};
+}
+  setInterval(reloadMessages, 5000);
 });
